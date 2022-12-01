b998bbf3120660e1a1f1485a4ac242454d5bca43
Kevin Price <collegekevin@gmail.com>
2022-12-01 08:50:23 -0800

    commit tree ece3ceaf0d67d37b98981edf57c5c40060ae0d23

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+def actors_list
+    self.actors.map do |actor|
+        #I originally tried self.characters.actors.map... I don't 
+        #have to do that. I already have acceess to actors. 
+        #No need to go through characters each time
+        actor.first_name + " " + actor.last_name
+        #I already did the first name in actor
+        #could have just done actor.full_name
+    end
+end
+
+
 end
